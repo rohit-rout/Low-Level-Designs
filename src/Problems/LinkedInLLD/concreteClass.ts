54cb8e2a89483dccdecb6cb46e131158eb0b5778
Rohit Rout <rohit@freshprints.com>
2025-03-02 16:40:57 +0530

    Adding profile builder in user !

--- src/Problems/LinkedInLLD/concreteClass.ts
+++ src/Problems/LinkedInLLD/concreteClass.ts
@@ -1,13 +1,59 @@
 import { UserFactory } from "./abstractClass.js";
+import { AuthStrategy, Profile } from "./interface.js";
 
 export class User {
-    constructor(public id: string , public type : 'basic' | 'premium') {}
+    constructor(public id: string , public type : 'basic' | 'premium', public email: string, public profile: Profile) {}
 
-     getProfile() {
-        console.log('user id is', this?.id);
+     describe() {
+        return this.profile.describe();
+     }
+
+     updateProfile(updateProfile: Profile) {
+        this.profile = updateProfile;
+     }
+}
+
+export class UserProfile implements Profile{
+    constructor(
+        public headline: string='',
+        public skills: string[]=[],
+        public summary : string ='',
+        public name : string = ''
+    ) {}
+
+    describe() {
+        console.log(`Hello ${this.name} you skill set includes ${this.skills} and your summary is ${this.summary} and your headline is ${this.headline}`);
     }
 }
 
+export class UserProfileBuilder {
+     profile = new UserProfile();
+
+     setSkills(skillSet: string[]) {
+         this.profile.skills.push(...skillSet);
+         return this;
+     }
+
+     setHeadline(headline: string) {
+         this.profile.headline = headline;
+         return this;
+     }
+
+     setSummary(summary: string) {
+         this.profile.summary = summary;
+         return this;
+     }
+
+     setName(name: string) {
+         this.profile.name = name;
+         return this;
+     }
+
+     build () {
+        return this.profile;
+     }
+
+}
 
 export class OAuthStrategy implements AuthStrategy {
     private validTokens =  new Set(['google','facebook','gmail']);
@@ -28,9 +74,14 @@ export class OAuthStrategy implements AuthStrategy {
     }
 
     register(credentials: any) {
-        if(!this.validate) throw new Error('Invalid token cannot register');
-        if(this.validTokens.has(credentials)) throw new Error('Token already exists');
-        this.validTokens.add(credentials);
+        return new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+                if(!this.validate) throw new Error('Invalid token cannot register');
+                if(this.validTokens.has(credentials)) throw new Error('Token already exists');
+                this.validTokens.add(credentials);
+            },1000)
+        })
+    
     }
 }
 
@@ -42,35 +93,52 @@ export class EmailPasswordStrategy implements AuthStrategy {
     }
     authenticate(credentials: any) {
         return new Promise<boolean | string>((resolve, reject)=>{
-            if(!this.validate) reject('Invalid credentials format cannot login');
-            const {email,password} = credentials;
-            const userPassword = this.userBase[email];
-            if(userPassword === password) resolve(true);
-            else reject('Wrong Email or Password');
+            setTimeout(()=>{
+                if(!this.validate) reject('Invalid credentials format cannot login');
+                const {email,password} = credentials;
+                const userPassword = this.userBase[email];
+                if(userPassword === password) {
+                    console.log('Successful authentication, logging the user');
+                    resolve(true);
+                }
+                else reject('Wrong Email or Password');
+            },1000)
         })
-       
      }
+
     validate(credentials: any) {
         if(typeof credentials === 'object' && 'email' in credentials && 'password' in credentials) return true;
         else return false;
      }
     register(credentials: any) {
-        if(!this.validate) throw new Error('Invalid credentials cannot register');
-        const {email,password} = credentials;
-        if(email in this.userBase) throw new Error('Email already exists');
-        this.userBase[email] = password;
-        console.log('register the user successfully');
+        return new Promise((resolve, reject)=>{
+            setTimeout(()=>{
+                if(!this.validate) throw new Error('Invalid credentials cannot register');
+                const {email,password} = credentials;
+                if(email in this.userBase) throw new Error('Email already exists');
+                this.userBase[email] = password;
+                console.log('register the user successfully');
+            },1000)
+        })
+   
     }
 }
 
 export class BasicUserFactory extends UserFactory {
     private idCounter = 0;
     
-    createUser(type: 'basic' | 'premium'): User {
-        this.idCounter++;
-        return new User(
-            `user_${this.idCounter}`,
-            type
-        );
+    createUser(info?:any): User {
+        try {
+            this.idCounter++;
+            return new User(
+                `user_${this.idCounter}`,
+                'basic',
+                info?.email,
+                new UserProfile()
+                );
+        } catch (error) {
+            console.log(error,'cannot create user');
+        }
+      
     }
 }
--- src/Problems/LinkedInLLD/interface.ts
+++ src/Problems/LinkedInLLD/interface.ts
@@ -1,5 +1,13 @@
-interface AuthStrategy {
+export interface AuthStrategy {
     authenticate(credentials: any) : Promise<string | boolean>;
     validate(credentials: any) : boolean;
     register(credentials: any): void;
+}
+
+export interface Profile {
+    name:string;
+    headline: string;
+    skills: string[];
+    summary: string;
+    describe():void;
 }
